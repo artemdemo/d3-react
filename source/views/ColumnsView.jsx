646dc9fb2f662700af8ad3ebdce7cc6cdac62fd7
Artem Demo <artemdemo@gmail.com>
2016-09-19 20:16:03 +0300

    updated <Chart> tag props

--- source/views/ColumnsView.jsx
+++ source/views/ColumnsView.jsx
@@ -17,7 +17,10 @@ export class ColumnsView extends Component {
             ['2017', 1030]
         ];
         return (
-            <Chart data={columns} className='columns-chart' width='100%' height='500'>
+            <Chart data={columns}
+                   className='columns-chart'
+                   width='100%'
+                   height='500'>
                 <Axis />
                 <Columns />
             </Chart>
